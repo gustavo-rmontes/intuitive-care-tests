2f2b925ea191c51fbe677a8628bbe613c24056bd
gustavo <gustavoribeiromontes@gmail.com>
2025-03-29 19:25:04 -0300

    [Teste-3] Analytical queries created

---
+++ teste3/ultimos-3_meses.sql
@@ -0,0 +1,14 @@
+WITH ultimos_3_meses AS
+    (SELECT d.reg_ans,
+            o.razao_social,
+            SUM(d.vl_saldo_final) AS total_despesas
+     FROM demo_contabeis d
+     JOIN operadoras_saude o ON d.reg_ans = o.reg_ans
+     WHERE d.descricao = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR'
+         AND d.data_lancamento >= (CURRENT_DATE - INTERVAL '3 months')
+     GROUP BY d.reg_ans,
+              o.razao_social)
+SELECT *
+FROM ultimos_3_meses
+ORDER BY total_despesas DESC
+LIMIT 10;
