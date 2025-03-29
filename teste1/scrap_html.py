447469b8bc0581b4a69a6d8c8de1563a7050acbe
gustavo <gustavoribeiromontes@gmail.com>
2025-03-29 14:04:54 -0300

    [Teste-1] Downloading files

--- teste1/scrap_html.py
+++ teste1/scrap_html.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
 gov_url = 'https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos'
 
@@ -17,3 +18,23 @@ for a in soup.find_all('a', href=True):
     href_link = a['href'] 
     if texto_link in texto_anexos and href_link.endswith('.pdf'):
         links_anexos.append(a['href'])
+
+# Cria a pasta 'anexos' se ela não existir
+os.makedirs("anexos", exist_ok=True)
+
+# Baixa os PDFs
+for link in links_anexos:
+    file_name = os.path.basename(link)
+    file_path = os.path.join("anexos", file_name)
+    print(f"Baixando {file_name}...")
+    
+    try:
+        pdf_response = requests.get(link, stream=True)
+        pdf_response.raise_for_status() 
+        
+        with open(file_path, 'wb') as f:
+            f.write(pdf_response.content)
+        print(f"{file_name} salvo!\n")
+    
+    except Exception as e:
+        print(f"Erro ao baixar {file_name}: {e}\n")
