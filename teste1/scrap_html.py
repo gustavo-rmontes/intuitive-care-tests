af36cc31e3bca115c4e0d112d0411c39b7019c79
gustavo <gustavoribeiromontes@gmail.com>
2025-03-29 14:04:07 -0300

    [Teste-1] Get the download references

--- teste1/scrap_html.py
+++ teste1/scrap_html.py
@@ -1,7 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
 gov_url = 'https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos'
 
 response = requests.get(url=gov_url) 
 print("[url] Response status: " + str(response.status_code))
 
+soup = BeautifulSoup(response.text, 'html.parser')
+
+links_anexos = []
+texto_anexos = ['Anexo I.', 'Anexo II.']
+
+# Gera a lista de links com .pdf e que tem Anexo no texto de <a>
+for a in soup.find_all('a', href=True):
+    texto_link = a.text 
+    href_link = a['href'] 
+    if texto_link in texto_anexos and href_link.endswith('.pdf'):
+        links_anexos.append(a['href'])
